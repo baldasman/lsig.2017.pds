eb88f49610dac38e8c432c11d69be4a672ab7136
Pedro Santos <baldasman@gmail.com>
2017-05-05 14:03:10 +0100

    Model manipulation example

--- JoiaDaEuropa/app/controllers/application_controller.rb
+++ JoiaDaEuropa/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!
+    before_action :check_lang
+
+
+    private
+
+    def check_lang
+
+        # todo: change lang
+
+    end
+
 end
--- JoiaDaEuropa/app/controllers/home_controller.rb
+++ JoiaDaEuropa/app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
 
-  def index
+    skip_before_action :authenticate_user!
 
+    def index
 
+        @today = Time.now
 
-  end
+    end
 
 
 end
